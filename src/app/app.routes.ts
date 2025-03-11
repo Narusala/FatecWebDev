b38afc7e964ea0d8fb786c9138361d5623fa9dbd
Nathan Lazzaro <rplazzaro@hotmail.com>
2025-03-11 18:33:51 -0300

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { FaleConoscoComponent } from './fale-conosco/fale-conosco.component';
+import { VitrineComponent } from './vitrine/vitrine.component';
+import path from 'path';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: "faleconosco", component: FaleConoscoComponent},
+    {path: "contato", redirectTo: "faleconosco"},
+    {path: "vitrine", component: VitrineComponent},
+    {path: "", component: VitrineComponent},
+
+];
